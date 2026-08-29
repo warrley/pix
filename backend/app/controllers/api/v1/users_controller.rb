64a954c22c3d8a94c56ba247141386c09af2cf3c
Gildean Morais <gildean.m098@gmail.com>
2026-08-29 13:03:52 -0300

    feat(Controller): add the routes, and controller endpoints, msg handlers and error handlers

---
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,44 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, only: [:show, :update, :destroy]
+
+      def show
+        render_success(@user)
+      end
+
+      def create
+        user = User.new(user_params)
+
+        if user.save
+          render_success(user, status: :created)
+        else
+          render_error(user.errors.to_hash)
+        end
+      end
+
+      def update
+        if @user.update(user_params)
+          render_success(@user)
+        else
+          render_error(@user.errors.to_hash)
+        end
+      end
+
+      def destroy
+        @user.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :doc_id, :phone)
+      end
+    end
+  end
+end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+
+  def render_success(data, status: :ok)
+    render json: { data: data, error: nil }, status: status
+  end
+
+  def render_error(error, status: :unprocessable_entity)
+    render json: { data: nil, error: error }, status: status
+  end
+
+  def render_not_found
+    render json: { data: nil, error: "Record not found" }, status: :not_found
+  end
 end
