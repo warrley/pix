1ed985db8a2e88c67e56ccf28af7cc638d5d4366
Gildean Morais <gildean.m098@gmail.com>
2026-08-29 11:45:55 -0300

    feat(Models): add the uniqueness constraint at user model and run the migration

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :doc_id, cpf_cnpj: true
+  validates :doc_id, cpf_cnpj: true, uniqueness: true
   validates :phone, format: { with: /\A\+55\d{10,11}\z/, message: "deve estar no formato E.164 (+55DDXXXXXXXXX)" },
                     allow_nil: true
 end
