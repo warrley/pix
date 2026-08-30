2bd11c9f7518b9cb594565469370138a10338e0e
Ryan <ryanlbbbb@gmail.com>
2026-08-30 12:38:11 -0300

    feat(controllers): add create and delete account actions

--- backend/app/controllers/api/v1/accounts_controller.rb
+++ backend/app/controllers/api/v1/accounts_controller.rb
@@ -1,23 +1,50 @@
 module Api
   module V1
     class AccountsController < ApplicationController
-      before_action :set_account, only: [:show]
+      before_action :set_account, only: [:show, :destroy]
 
-      # GET /api/v1/users/:user_id/accounts
+      def create
+        account = Account.new(account_params)
+
+        if account.save
+          render_success(account, status: :created)
+        else
+          render_error(account.errors.to_hash)
+        end
+      end
+      
       def index
         user = User.find(params[:user_id])
         render_success(user.accounts)
       end
 
-      # GET /api/v1/accounts/:id
       def show
         render_success(@account)
+      end
+
+      def destroy
+        if @account.status == "closed"
+          return render_error({ error: "Account is already closed"}, status: :unprocessable_entity)
+        end
+        if @account.balance > 0 
+          return render_error({error: "Cannot close account with existing balance"}, status: :bad_request)
+        end
+
+        if @account.update(status: "closed")
+          head :no_content
+        else
+          render_error(@account.errors.to_hash)
+        end
       end
 
       private
 
       def set_account
         @account = Account.find(params[:id])
+      end
+
+      def account_params
+        params.require(:account).permit(:user_id, :agency_number, :balance, :status)
       end
     end
   end
